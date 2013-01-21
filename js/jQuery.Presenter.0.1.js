f30a4f0ea35a9cbe8ed76051f7a264a0cf458dee
aa_kuznetsov <isnifer@gmail.com>
2013-01-21 13:58:29 +0400

    Add from friday

--- js/jQuery.Presenter.0.1.js
+++ js/jQuery.Presenter.0.1.js
@@ -8,13 +8,11 @@
 
     that.slides = (slide !== undefined) ? document.querySelectorAll(slide) : document.querySelectorAll('.slide');
     that.slideLen = this.slides.length;
-    that.current = (localStorage.currentSlide !== undefined) ? localStorage.currentSlide : 'main';
+    that.current = (localStorage.currentSlide === 'undefined') ? 'main' : localStorage.currentSlide ;
+    localStorage.setItem('currentSlide', that.current);
 
 
-    console.log('Текущий слайд', that.current);
-
-
-    /*
+    /**
     * Get ResizeRatio for Body
     *
     * @returns {String}
@@ -47,24 +45,98 @@
     that._bodyTransform(that._getRatio());
 
 
+    /**
+    * Check if body class="list"
+    *
+    * Return {Boolean}
+    * */
 
-    // Вешаем обработчики событий
+    that._isView = function(){
+      return !!(($(body).attr('class') === 'view'));
+    };
+
+    /**
+    * Change body class view or list
+    * @private
+    * Return {string}
+    * */
+    that._updateBodyState = function(){
+      body.className = (that._isView() === true) ? 'list' : 'view';
+    };
 
 
-    //Scale Presenter when we Resize window
+    /**
+    * Update Slide Number
+    * @private
+    *
+    * */
+    that._updateSlideNum = function(){
+      var numpage = (localStorage.currentSlide === 'main') ? '' : localStorage.currentSlide;
+      $('#numpage').html(numpage);
+    };
+
+    /**
+    * Update percent slider
+    * @private
+    * */
+    that._updatePercentSlider = function(){
+      var progressPercent = ((localStorage.currentSlide === 'main') ? 100 / that.slideLen : 100 / that.slideLen * (localStorage.currentSlide + 1));
+      $('#bar').css('width', progressPercent);
+    };
+
+    // Event Handlers
+
+
+    // Scale Presenter when we Resize window
     $(window).bind('resize', function(){
       that._bodyTransform(that._getRatio());
     });
 
-    $('#' + localStorage.currentSlide).addClass('active');
-
+    /**
+    * Binding event click and arrows
+    *
+    * */
     $('#next').on('click', function(e){
       e.preventDefault();
       var currentSlide = $('#' + localStorage.currentSlide);
-      currentSlide.removeClass('active');
+          currentSlide.removeClass('active');
+
       var current = (currentSlide.attr('id') === $('.slide:last').attr('id')) ? 'main' : currentSlide.next().attr('id');
       localStorage.setItem('currentSlide', current);
+
       $('#' + localStorage.currentSlide).addClass('active');
+      that._updatePercentSlider();
+      that._updateSlideNum();
+
+    });
+
+    /**
+     * Binding keydown for left(37), up(38), right(39), down(40), Esc(27)
+     *
+     */
+    document.addEventListener('keydown', function(e){
+      e.preventDefault();
+      switch (e.keyCode){
+        case 37:
+          console.log('left');
+          break;
+        case 38:
+          console.log('top');
+          break;
+        case 39:
+          console.log('right');
+          that._updatePercentSlider();
+          that._updateSlideNum();
+          break;
+        case 40:
+          console.log('bottom');
+          break;
+        case 27:
+          that._updateBodyState();
+          break;
+        default:
+          return false;
+      }
     });
 
   };
